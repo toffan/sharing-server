d0d20336bdabb3bb08310716cd32e1020cce554a
Patathor <guilboj@etu.enseeiht.fr>
2015-12-27 12:49:57 +0100

    Change ArrayList to HashMap, and HashMap<String, ServerObject> to HashMap<String, Integer>
    Change ArrayList to HashMap to avoid almost empty list, and change
    HashMap<String, ServerObject> to HashMap<String, Integer> because the
    client side does not need the object, but only the ID.

--- src/Server.java
+++ src/Server.java
@@ -7,41 +7,42 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Server extends UnicastRemoteObject implements Server_itf {
 
-    private ArrayList<ServerObject> s_objs;
-    private HashMap<String, ServerObject> hm_name;
+    private HashMap<Integer, ServerObject> s_objs;
+    private HashMap<String, Integer> hm_name;
     private ReentrantLock mutex;
+    private int cpt;
 
     public Server() throws RemoteException {
-        hm_name = new HashMap<String, ServerObject>();
-        s_objs = new ArrayList<ServerObject>();
+        hm_name = new HashMap<String, Integer>();
+        s_objs = new HashMap<Integer, ServerObject>();
         mutex = new ReentrantLock();
+        cpt = -1;
     }
 
     public int lookup(String name) throws java.rmi.RemoteException {
         mutex.lock();
-        ServerObject s_obj = hm_name.get(name);
+        Integer id = hm_name.get(name);
         mutex.unlock();
 
-        if(s_obj == null)
-            return -1;
-        else
-            return s_obj.getId();
+        return (id == null) ? -1 : id;
     }
 
     public void register(String name, int id) throws java.rmi.RemoteException {
         mutex.lock();
-        hm_name.put(name, s_objs.get(id));
+        hm_name.put(name, id);
         mutex.unlock();
     }
 
     public int create(Object o) throws java.rmi.RemoteException {
         mutex.lock();
-        int id = s_objs.size();
-        ServerObject s_obj = new ServerObject(id, o);
-        s_objs.add(s_obj);
+        do {
+            cpt++;
+        } while(s_objs.get(cpt) != null);
+        ServerObject s_obj = new ServerObject(cpt, o);
+        s_objs.put(cpt, s_obj);
         mutex.unlock();
 
-        return id;
+        return cpt;
     }
 
     public Object lock_read(int id, Client_itf client) throws java.rmi.RemoteException {
