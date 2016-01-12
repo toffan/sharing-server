2f68995b4cdf6bbb9605a0fe54c897091865a4c6
toffan <benoit.lemarchand@inpt.fr>
2016-01-12 20:43:02 +0100

    Clean up

--- src/ServerObject.java
+++ src/ServerObject.java
@@ -31,48 +31,49 @@ public class ServerObject {
     }
 
     public synchronized void lock_read(Client_itf c) {
-        assert(c != null);
+        assert (c != null);
 
-        if(s_state == SState.WLT) {
-            assert(c_locks.size() == 1);
+        if (s_state == SState.WLT) {
+            assert (c_locks.size() == 1);
             try {
-                if(!c_locks.getFirst().equals(c))
-                {
+                if (!c_locks.getFirst().equals(c)) {
                     this.obj = c_locks.getFirst().reduce_lock(id);
                 }
-            } catch(RemoteException e) {
+            }
+            catch (RemoteException e) {
                 throw new RuntimeException(e);
             }
         }
 
         s_state = SState.RLT;
 
-        if(!c_locks.contains(c))
+        if (!c_locks.contains(c)) {
             c_locks.add(c);
+        }
     }
 
     public synchronized void lock_write(Client_itf c) {
-        assert(c != null);
+        assert (c != null);
 
-        if(s_state == SState.WLT) {
-            assert(c_locks.size() == 1);
+        if (s_state == SState.WLT) {
+            assert (c_locks.size() == 1);
             try {
-                if(!c_locks.getFirst().equals(c)) {
+                if (!c_locks.getFirst().equals(c)) {
                     this.obj = c_locks.getFirst().invalidate_writer(id);
                 }
             }
-            catch(RemoteException e) {
+            catch (RemoteException e) {
                 throw new RuntimeException(e);
             }
         }
-        else if(s_state == SState.RLT) {
-            for(Client_itf c_lock: c_locks) {
+        else if (s_state == SState.RLT) {
+            for (Client_itf c_lock: c_locks) {
                 try {
-                    if(!c_lock.equals(c)) {
+                    if (!c_lock.equals(c)) {
                         c.invalidate_reader(id);
                     }
                 }
-                catch(RemoteException e) {
+                catch (RemoteException e) {
                     throw new RuntimeException(e);
                 }
             }
