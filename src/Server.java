141988858e2a04db38c1798724fe1df61271a6bf
toffan <benoit.lemarchand@inpt.fr>
2016-01-25 18:20:34 +0100

    Change the king of lock used
    Replace a ReentrantLock by a ReentrantReadWriteLock.

--- src/Server.java
+++ src/Server.java
@@ -6,7 +6,7 @@ import java.rmi.server.UnicastRemoteObject;
 import java.net.MalformedURLException;
 
 import java.util.HashMap;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 
 public class Server extends UnicastRemoteObject implements Server_itf {
@@ -15,54 +15,54 @@ public class Server extends UnicastRemoteObject implements Server_itf {
 
     private HashMap<Integer, ServerObject> s_objs;
     private HashMap<String, Integer> hm_name;
-    private ReentrantLock mutex;
+    private ReentrantReadWriteLock mutex;
     private int cpt;
 
     public Server() throws RemoteException {
         hm_name = new HashMap<String, Integer>();
         s_objs = new HashMap<Integer, ServerObject>();
-        mutex = new ReentrantLock();
+        mutex = new ReentrantReadWriteLock();
         cpt = -1;
     }
 
     public int lookup(String name) throws java.rmi.RemoteException {
-        mutex.lock();
+        mutex.readLock().lock();
         Integer id = hm_name.get(name);
-        mutex.unlock();
+        mutex.readLock().unlock();
 
         return (id == null) ? -1 : id;
     }
 
     public void register(String name, int id) throws java.rmi.RemoteException {
-        mutex.lock();
+        mutex.writeLock().lock();
         hm_name.put(name, id);
-        mutex.unlock();
+        mutex.writeLock().unlock();
     }
 
     public int create(Object o) throws java.rmi.RemoteException {
-        mutex.lock();
+        mutex.writeLock().lock();
         do {
             cpt++;
         } while(s_objs.get(cpt) != null);
         ServerObject s_obj = new ServerObject(cpt, o);
         s_objs.put(cpt, s_obj);
-        mutex.unlock();
+        mutex.writeLock().unlock();
 
         return cpt;
     }
 
     public Object lock_read(int id, Client_itf client) throws java.rmi.RemoteException {
-        mutex.lock();
+        mutex.readLock().lock();
         ServerObject obj = s_objs.get(id);
-        mutex.unlock();
+        mutex.readLock().unlock();
         obj.lock_read(client);
         return obj.get_obj();
     }
 
     public Object lock_write(int id, Client_itf client) throws java.rmi.RemoteException {
-        mutex.lock();
+        mutex.readLock().lock();
         ServerObject obj = s_objs.get(id);
-        mutex.unlock();
+        mutex.readLock().unlock();
         obj.lock_write(client);
         return obj.get_obj();
     }
